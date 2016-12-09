5ed9c7487c2546e9aff7bc74d42f3147ec30b027
sarahboka <boka.sarah@gmail.com>
2016-12-09 14:16:51 -0500

    adding security

--- DBScript.sql
+++ DBScript.sql
@@ -3,7 +3,7 @@ CREATE DATABASE VendingMachine;
 
 USE VendingMachine;
 
-CREATE TABLE Items(
+CREATE TABLE IF NOT EXISTS Items(
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(30) NOT NULL,
 	`cost` DECIMAL(10,4),
@@ -27,5 +27,45 @@ INSERT INTO `Items` (`id`, `name`, `cost`, `inventory`)
 					(13, "Oreos", 3.88, 4),
 					(14, "Chocolate Bar", 2.5, 3);
 
-
+--
+-- Table structure for table `users`
+--
+CREATE TABLE IF NOT EXISTS `users` (
+ `user_id` int(11) NOT NULL AUTO_INCREMENT,
+ `username` varchar(20) NOT NULL,
+ `password` varchar(20) NOT NULL,
+ `enabled` tinyint(1) NOT NULL,
+ PRIMARY KEY (`user_id`),
+ KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+--
+-- Dumping data for table `users`
+--
+INSERT INTO `users` (`user_id`, `username`, `password`, `enabled`) VALUES
+(1, 'admin', 'password', 1),
+(2, 'user', 'password', 1),
+(3, 'both', 'password', 1),
+(4, 'neither', 'password', 1);
+--
+-- Table structure for table `authorities`
+--
+CREATE TABLE IF NOT EXISTS `authorities` (
+ `username` varchar(20) NOT NULL,
+ `authority` varchar(20) NOT NULL,
+ KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Dumping data for table `authorities`
+--
+INSERT INTO `authorities` (`username`, `authority`) VALUES
+('admin', 'ROLE_ADMIN'),
+('user', 'ROLE_USER'),
+('both', 'ROLE_ADMIN'),
+('both', 'ROLE_USER'),
+('neither', 'EATS_POTATOES');
+--
+-- Constraints for table `authorities`
+--
+ALTER TABLE `authorities`
+ ADD CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
 
