a859d660ec8127bb093780ccca4bf44b13e3b590
Sarah Boka <boka.sarah@gmail.com>
2017-05-19 21:01:56 -0400

    can edit image and dimensions

--- src/main/webapp/js/adminJS.js
+++ src/main/webapp/js/adminJS.js
@@ -27,6 +27,30 @@ adApp.controller('adminController', ['$scope', '$log', '$http', '$sce', function
                     $("#edit-item-name").val($scope.item.name);
                     $("#edit-item-cost").val($scope.item.cost);
                     $("#edit-item-inv").val($scope.item.inventory);
+                    var imgStr = $scope.item.image;
+                    $log.log(imgStr);
+                    var len = imgStr.length;
+                    var quotes = 0;
+                    var src = "";
+                    var height = "";
+                    var width = "";
+                    for (var i = 0; i < len; i++){
+                        if (imgStr.charAt(i)==='"'){
+                            quotes += 1;
+                        }else if (quotes === 1){
+                            src += imgStr.charAt(i);
+                        }else if (quotes === 3){
+                            height += imgStr.charAt(i);
+                        }else if (quotes === 5){
+                            width += imgStr.charAt(i);
+                        }
+                    }
+                    $log.log(src);
+                    $log.log(height);
+                    $log.log(width);
+                    $("#edit-item-img").val(src);
+                    $("#img-edit-height").val(height);
+                    $("#img-edit-width").val(width);
                     
                 });
     };
@@ -63,20 +87,7 @@ adApp.controller('adminController', ['$scope', '$log', '$http', '$sce', function
 //    };
     
 $(document).ready(function(){
-    
-//    loadItems();
-    
-//    $("#add-button").click(function(event){
-//        event.preventDefault();
-//        addItem();
-//    });
-    
-//    $("#item-edit-modal").on('show.bs.modal', function(event){
-//        var element = $(event.relatedTarget);
-//        var itemId = element.data('item-id');
-//        itemEditDetails(itemId);
-//    });
-//    
+
     $("#edit-button").click(function(event){
         event.preventDefault();
         editItem();
@@ -84,88 +95,16 @@ $(document).ready(function(){
     
 });
 
-//function loadItems(){
-//    
-//    $.ajax({
-//        url : 'items',
-//        type : 'GET'
-//    }).success(function(data){
-//        processItemList(data);
-//    });
-//}
-//
-//function clearTable(){
-//    $('#itemListRows').empty();
-//    
-//}
-//
-//function processItemList(items){
-//    
-//    clearTable(); 
-//
-//    var itemListRows = $('#itemListRows');
-//
-//    $.each(items, function(index, item){
-//        
-//        var nameField = $("<td>");
-//        var costField = $("<td>");
-//        var invField = $("<td>");
-//        var editField = $("<td>");
-//        var deleteField = $("<td>");
-//        
-//        nameField.text(item.name);
-//        costField.text(item.cost);
-//        invField.text(item.inventory);
-//        
-//        var editLink = $("<a>");
-//        editLink.attr({
-//            'data-toggle': 'modal',
-//            'data-target': '#item-edit-modal',
-//            'data-item-id' : item.id
-//        });
-//        editLink.text("Edit");
-//        editField.append(editLink);
-//        
-//        var deleteLink = $("<a>");
-//        deleteLink.attr({
-//            'onclick': 'deleteItem('+item.id+')'
-//        });
-//        deleteLink.text("Delete");
-//        deleteField.append(deleteLink);
-//        
-//        var itemListRow = $("<tr>");
-//        itemListRow.append(nameField);
-//        itemListRow.append(costField);
-//        itemListRow.append(invField);
-//        itemListRow.append(editField);
-//        itemListRow.append(deleteField);
-//        
-//        itemListRows.append(itemListRow);
-//        
-//    });
-//}
-//
-//function itemEditDetails(id){
-//    $.ajax({
-//        type: 'GET',
-//        url: 'item/' + id,
-//        headers: {
-//            'Accept': 'application/json'
-//        }
-//    }).success(function(item){
-//        $("#edit-item-id").text(item.id);
-//        $("#edit-item-name").val(item.name);
-//        $("#edit-item-cost").val(item.cost);
-//        $("#edit-item-inv").val(item.inventory);
-//    });
-//}
-
 function editItem(){
     var id = $("#edit-item-id").text();
     var itemName = $("#edit-item-name").val();
     var itemCost = $("#edit-item-cost").val();
     var itemInv = $("#edit-item-inv").val();
+    var itemImg = $("#edit-item-img").val();
+    var imageHeight = $("#img-edit-height").val();
+    var imageWidth = $("#img-edit-width").val();
     
+    var imageString = '<img src="' + itemImg + '" height="' + imageHeight + '" width="' + imageWidth + '"/>';
     $.ajax({
         url: 'item/'+ id,
         type: 'PUT',
@@ -176,60 +115,10 @@ function editItem(){
         data : JSON.stringify({
             name : itemName,
             cost : itemCost,
-            inventory : itemInv
+            inventory : itemInv,
+            image: imageString
         })
     }).success(function(data){
         location.reload();
     });
 }
-
-function addItem(){
-    var itemName = $("#add-name").val();
-    var itemCost = $("#add-cost").val();
-    var itemInv = $("#add-inventory").val();
-    
-    $.ajax({
-        url : 'item',
-        type : 'POST',
-        headers: {
-            'Accept' : 'application/json',
-            'Content-Type' : 'application/json'
-        },
-        'data-Type' : 'json',
-        data : JSON.stringify({
-            name : itemName,
-            cost : itemCost,
-            inventory : itemInv
-        })
-    }).success(function(data){
-        location.reload();
-        
-        $("#validationErrors").hide();
-
-        $("#add-name").val('');
-        $("#add-cost").val('');
-        $("#add-inventory").val('');
-    }).error(function(data,status){
-        var errorDiv = $("#validationErrors");
-        errorDiv.empty();
-        errorDiv.show();
-        
-        $.each(data.responseJSON.fieldErrors, function(index, validationError){
-            errorDiv.append(validationError.message);
-            errorDiv.append("<br>");
-        });
-    });
-}
-
-//function deleteItem(id){
-//    var answer = confirm("Do you really want to delete this item?");
-// 
-//    if (answer === true) {
-//        $.ajax({
-//            type : 'DELETE',
-//            url : 'item/' + id
-//        }).success(function(){
-//            loadItems();
-//        });
-//    }
-//}
--- src/main/webapp/jsp/editModalFragment.jsp
+++ src/main/webapp/jsp/editModalFragment.jsp
@@ -7,7 +7,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <h3 id="edit-item-id"></h3>
+                <h5 id="edit-item-id"></h5>
                 <form class="form-horizontal" role="form">
                     <div class="form-group">
                         <label for="edit-item-name" class="col-md-4 control-label">Name:</label>
@@ -27,7 +27,21 @@
                             <input id="edit-item-inv" name="itemEditInv" type="text" class="form-control" placeholder="0"/>
                         </div>
                     </div>
-                    
+                    <div class="form-group">
+                        <label for="edit-item-img" class="col-md-4 control-label">Image url:</label>
+                        <div class="col-md-8">
+                            <input id="edit-item-img" name="itemEditImg" type="text" class="form-control" placeholder="http://..."/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit-item-img" class="col-md-4 control-label">Dimensions:</label>
+                        <div class="col-md-4">
+                            Height: <input id="img-edit-height" name="imgEditHeight" type="text" class="form-control" placeholder="height"/>
+                        </div>
+                        <div class="col-md-4">
+                            Width: <input id="img-edit-width" name="imgEditWidth" type="text" class="form-control" placeholder="width"/>
+                        </div>
+                    </div><br><br>
                     <div class="form-group">
                         <div class="col-md-offset-4 col-md-4">
                             <button id="edit-button" class="btn btn-primary" data-dismiss="modal">Edit Item</button>
